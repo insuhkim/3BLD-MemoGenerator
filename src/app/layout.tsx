49f8271595c697bf08272db98d4a84705176f17c
insuhkim <insuhkim@naver.com>
2025-05-11 22:57:32 +0900

    feat: Add title, description metadata in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./styles/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Memo Generator",
+  description: "Memo Generator for 3x3 blindfolded",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
