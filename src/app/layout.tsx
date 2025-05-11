5cf2db1d278fa9ae01e08799395641a91ad0824d
insuhkim <insuhkim@naver.com>
2025-05-11 22:57:56 +0900

    design: Change icon into eyemask

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Memo Generator",
   description: "Memo Generator for 3x3 blindfolded",
+  icons: {
+    icon: "/icons/eyemask.png",
+  },
 };
 
 export default function RootLayout({
