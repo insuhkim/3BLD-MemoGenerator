c15229ea492a36a350f531032e92c024f4b6099c
insuhkim <insuhkim@naver.com>
2025-05-12 23:37:45 +0900

    refactor: Update edge solving logic and enhance result display in Memo component

--- src/app/components/Memo.tsx
+++ src/app/components/Memo.tsx
@@ -1,14 +1,33 @@
 import { applyScramble, Cube, DisplayCube } from "react-rubiks-cube-utils";
-import { solveEdge } from "../scripts/memoEdge";
+import { solveEdges } from "../scripts/memoEdge";
 
 export default function Memo({ scramble }: { scramble: string }) {
   const cube = applyScramble({ type: "3x3", scramble: scramble });
-  // const edge = solveEdge(cube, "C");
+  const edge = solveEdges(cube, "C");
+  let edgeString = "";
+  let count = 0;
+  for (let i = 0; i < edge.length; i++) {
+    edgeString += "(";
+    for (let j = 0; j < edge[i].length; j++) {
+      if (j > 0 && count % 2 === 0) edgeString += ",";
+      edgeString += edge[i][j];
+      count++;
+    }
+    edgeString += ")";
+  }
+  const isParity = count % 2 === 1;
+  console.log("isParity", isParity);
+  console.log("edgeString", edgeString);
+  console.log("result", edge);
   return (
     <div>
-      <h1> Results </h1>
       <div>
         <DisplayCube cube={cube} size={28} />
+      </div>
+      <h1> Results </h1>
+      <div>
+        {isParity && <h3>Parity</h3>}
+        <p>{edgeString} </p>
       </div>
     </div>
   );
