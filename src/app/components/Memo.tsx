f19c31de57adb4bb677930f9f1d64b744c244fac
insuhkim <insuhkim@naver.com>
2025-05-14 02:06:21 +0900

    feat: Add edge and corner priority handling from localStorage in Memo component

--- src/app/components/Memo.tsx
+++ src/app/components/Memo.tsx
@@ -2,16 +2,38 @@ import { applyScramble, Cube } from "react-rubiks-cube-utils";
 import solveEdges from "../scripts/memoEdge";
 import solveCorners from "../scripts/memoCorner";
 import makeLetterpair from "../scripts/makeLetterpair";
+import { Speffz } from "../scripts/Speffz";
 
 export default function Memo({ scramble }: { scramble: string }) {
+  // Read priorities from localStorage
+  const edgePriority =
+    typeof window !== "undefined"
+      ? localStorage.getItem("edgePriority") || ""
+      : "";
+  const cornerPriority =
+    typeof window !== "undefined"
+      ? localStorage.getItem("cornerPriority") || ""
+      : "";
+  const regex = /^[A-X]*$/;
+
   const cube = applyScramble({ type: "3x3", scramble: scramble });
-  const edge = solveEdges(cube, "C");
-  const corner = solveCorners(cube, "C");
+  const edge = solveEdges(
+    cube,
+    "C",
+    edgePriority.split(" ").filter((e) => e !== "" && regex.test(e)) as Speffz[]
+  );
+  const corner = solveCorners(
+    cube,
+    "C",
+    cornerPriority
+      .split(" ")
+      .filter((e) => e !== "" && regex.test(e)) as Speffz[]
+  );
   const [edgeString, parityEdge] = makeLetterpair(edge);
   const [cornerString, parityCorner] = makeLetterpair(corner);
   if (parityCorner !== parityEdge) {
     throw new Error("Parity error: edges and corners have different parity");
-  }
+  } //
   const isParity = parityEdge;
   return (
     <div>
