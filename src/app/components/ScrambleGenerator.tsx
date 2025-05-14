f096bbacfcbf5037cd236968f45630d3af70c9b5
insuhkim <insuhkim@naver.com>
2025-05-14 13:42:40 +0900

    feat: Replace input with textarea for adjust height dynamically

--- src/app/components/ScrambleGenerator.tsx
+++ src/app/components/ScrambleGenerator.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRef } from "react";
 import { generateScramble } from "react-rubiks-cube-utils";
 export default function scrambleGenerator({
   scramble,
@@ -6,28 +8,48 @@ export default function scrambleGenerator({
   scramble: string;
   setScramble: (scramble: string) => void;
 }) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   const handleClick = () => {
+    const el = textareaRef.current;
+    if (el) {
+      el.style.height = "auto"; // Reset height
+      el.style.height = `${el.scrollHeight}px`; // Set to content height
+    }
+
     setScramble(
       generateScramble({
         type: "3x3",
       })
     );
   };
+
   return (
     <div>
-      <input
-        type="text"
-        value={scramble}
-        onChange={(e) => {
-          const value = e.target.value;
-          // value only contains valid moves
-          const regex = /^[UDFBLR2' ]*$/;
-          if (regex.test(value)) {
-            setScramble(value);
-          }
-        }}
-        placeholder="Enter scramble"
-      />
+      <div>
+        <label htmlFor="scramble">Enter scramble or Generate Scramble</label>
+        <textarea
+          id="scramble"
+          ref={textareaRef}
+          rows={1}
+          placeholder="Enter scramble"
+          style={{
+            overflow: "hidden",
+            resize: "none",
+            width: "100%",
+            padding: "12px",
+            fontSize: "1rem",
+            lineHeight: "1.5",
+          }}
+          value={scramble}
+          onChange={(e) => {
+            const value = e.target.value;
+            const regex = /^[UDFBLR2' ]*$/;
+            if (regex.test(value)) {
+              setScramble(value);
+            }
+          }}
+        ></textarea>
+      </div>
       <br />
       <button onClick={handleClick}> Generate Scramble </button>
     </div>
