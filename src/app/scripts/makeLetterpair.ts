04c51614ba7d7c7f67af9965eb94907a3472f69a
insuhkim <insuhkim@naver.com>
2025-05-14 23:45:14 +0900

    feat: Add seperator and showCycle in makeLetterpair function

--- src/app/scripts/makeLetterpair.ts
+++ src/app/scripts/makeLetterpair.ts
@@ -2,18 +2,19 @@ import { Speffz } from "./Speffz";
 
 export default function makeLetterpair(
   memo: Speffz[][],
-  seperator: string = ","
+  seperator: string = ", ",
+  showCycle: boolean = true
 ): [string, boolean] {
   let memoString = "";
   let count = 0;
   for (let i = 0; i < memo.length; i++) {
-    memoString += "(";
+    if (showCycle) memoString += "(";
     for (let j = 0; j < memo[i].length; j++) {
       if (j > 0 && count % 2 === 0) memoString += seperator;
       memoString += memo[i][j];
       count++;
     }
-    memoString += ")";
+    if (showCycle) memoString += ")";
   }
   const isParity = count % 2 === 1;
   return [memoString, isParity];
