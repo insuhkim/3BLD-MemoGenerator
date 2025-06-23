e86c41b8952715fa3981ce9084683ed3e2d094fd
insuhkim <insuhkim@naver.com>
2025-06-23 12:46:24 +0900

    feat: Implement url to blddb

---
+++ src/utils/BLDDB/CornerToURL.ts
@@ -0,0 +1,36 @@
+import { speffzToCorner } from "../makeMemo/makeCornerMemo";
+import { Speffz } from "../types/Speffz";
+
+function SpeffzCornerToPosition(corner: Speffz): string {
+  const [orientedCorner, rotation] = speffzToCorner(corner);
+  const face = orientedCorner[rotation];
+  const face1 = orientedCorner[(rotation + 1) % 3];
+  const face2 = orientedCorner[(rotation + 2) % 3];
+  const priority: Record<"U" | "D" | "F" | "B" | "R" | "L", number> = {
+    U: 0,
+    D: 0,
+    F: 1,
+    B: 1,
+    R: 2,
+    L: 2,
+  };
+  return (
+    face +
+    (priority[face1 as keyof typeof priority] <
+    priority[face2 as keyof typeof priority]
+      ? face1 + face2
+      : face2 + face1)
+  );
+}
+
+export default function CornerToURL(
+  buffer: Speffz,
+  position1: Speffz,
+  position2: Speffz
+): string {
+  return `https://v2.blddb.net/corner?position=${SpeffzCornerToPosition(
+    buffer
+  )}-${SpeffzCornerToPosition(position1)}-${SpeffzCornerToPosition(
+    position2
+  )}&mode=manmade`;
+}
---
+++ src/utils/BLDDB/EdgeToURL.ts
@@ -0,0 +1,19 @@
+import { speffzToEdge } from "../makeMemo/makeEdgeMemo";
+import { Speffz } from "../types/Speffz";
+
+function SpeffzEdgeToPosition(edge: Speffz): string {
+  const [orientedEdge, isFlipped] = speffzToEdge(edge);
+  return isFlipped ? orientedEdge.split("").reverse().join("") : orientedEdge;
+}
+
+export default function Edge2URL(
+  buffer: Speffz,
+  position1: Speffz,
+  position2: Speffz
+): string {
+  return `https://v2.blddb.net/edge?position=${SpeffzEdgeToPosition(
+    buffer
+  )}-${SpeffzEdgeToPosition(position1)}-${SpeffzEdgeToPosition(
+    position2
+  )}&mode=manmade`;
+}
