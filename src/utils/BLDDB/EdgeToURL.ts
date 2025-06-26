5a078a77da90e84b7f21cb63a21fe804f14f112f
insuhkim <insuhkim@naver.com>
2025-06-26 22:51:01 +0900

    feat: Implement Edge flip url

--- src/utils/BLDDB/EdgeToURL.ts
+++ src/utils/BLDDB/EdgeToURL.ts
@@ -6,6 +6,17 @@ function SpeffzEdgeToPosition(edge: Speffz): string {
   return isFlipped ? orientedEdge.split("").reverse().join("") : orientedEdge;
 }
 
+export function SpeffzEdgeToOrientedPosition(edge: Speffz): string {
+  const [orientedEdge, isFlipped] = speffzToEdge(edge);
+  return isFlipped ? orientedEdge.split("").reverse().join("") : orientedEdge;
+}
+
+export function EdgeFlipURL(position1: Speffz, position2: Speffz): string {
+  return `https://v2.blddb.net/flips?position=${SpeffzEdgeToOrientedPosition(
+    position1
+  )}-${SpeffzEdgeToOrientedPosition(position2)}&mode=flip`;
+}
+
 export function Edge2URL(
   buffer: Speffz,
   position1: Speffz,
